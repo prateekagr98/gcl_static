7d3b2c195603610afaaa3e77fbd018c32f8a8677
Prateek Agarwal <prateekagr98@gmail.com>
2018-11-29 18:41:41 +0530

    Add side popup for all solutions

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
 $(document).ready(function(){
 
+	var shown = false;
+
+	if($('.popup--container')) {
+	  $('.popup--container').hide();
+	  $('.sidepopup--container').hide();
+	  $(window).scroll(function() {
+	     if($(window).scrollTop() + $(window).height() == $(document).height()) {
+	      if(!shown){
+	        $('.sidepopup--container').fadeOut();
+	        $('.popup--container').fadeIn();
+	        shown = true;
+	      }
+	     }
+	  });
+
+	  $('.sidepopup--container').on('click', function(e) {
+	    $('.popup--container').fadeIn();
+	    $('.sidepopup--container').fadeOut();
+	  });
+
+	  $('.popup__close').on('click', function(){
+	    $('.popup--container').fadeOut();
+	    $('.sidepopup--container').fadeIn();
+	  });
+	 }
+
 	$('#subsribe-submit').on('click', function(){
 		var payload = {};
 
